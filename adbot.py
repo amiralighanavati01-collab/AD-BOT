5c42571cec8556c1ca0c6bbdd15262b8b9e27cc2
amiralighanavati01-collab <amiralighanavati01@gmail.com>
2026-08-08 02:10:11 +0330

    Update adbot.py

--- adbot.py
+++ adbot.py
@@ -1,3 +1,4 @@
+مک
 from highrise import BaseBot, User, Position
 from highrise.__main__ import BotDefinition
 from asyncio import sleep, create_task, CancelledError
@@ -23,8 +24,8 @@ logger = logging.getLogger(__name__)
 # تنظیمات پیش‌فرض
 CONFIG_FILE = "bot_config.json"
 DEFAULT_CONFIG = {
-    "host_usernames": ["ad0ri"],
-    "admin_usernames": ["ad0ri"],
+    "host_usernames": ["popo_768"],
+    "admin_usernames": ["popo_768"],
     "vip_usernames": [],
     "banned_users": [],
     "teleport_locations": {
